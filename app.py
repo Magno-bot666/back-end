66f9d8bcf639dec0631e4bf16e54dd2107b2faa3
Magno-bot666 <o.magno@escola.pr.gov.br>
2025-06-06 07:55:25 -0300

    a

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/acesso', methods=['POST'])
+def acesso():
+    username = request.form['username']
+    password = request.form['password']
+    
+    if username == 'gabriel' and password == '123':
+        return redirect('/servicos')
+    else:
+        print('Usuário não encontrado')
+        return "Usuário ou senha incorretos", 401
+
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadstro.html')
+
+@app.route('/servicos')
+def servicos():
+    return render_template('servicos.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
